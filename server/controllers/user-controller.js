11d54d86a40d496302984458dc9d2ddf728cab14
Joshua <Joshuahale829@gmail.com>
2024-05-02 07:42:57 -0500

    Copied over user-controller code from previous assignment. Commented out but left potentially useless code so that it can rewritten later if need be.

---
+++ server/controllers/user-controller.js
@@ -0,0 +1,75 @@
+// import user model
+const { User } = require('../models');
+
+// import sign token function from auth
+const { signToken } = require('../utils/auth'); // Creates a JWT with the user's data and signs it with a secret-key.
+
+module.exports = {
+  // get a single user by either their id or their username
+  async getSingleUser({ user = null, params }, res) {
+    const foundUser = await User.findOne({
+      $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
+    });
+
+    if (!foundUser) {
+      return res.status(400).json({ message: 'Cannot find a user with this id!' });
+    }
+
+    res.json(foundUser);
+  },
+  // create a user, sign a token, and send it back (to client/src/components/SignUpForm.js)
+  async createUser({ body }, res) {
+    const user = await User.create(body);
+
+    if (!user) {
+      return res.status(400).json({ message: 'Something is wrong!' });
+    }
+    const token = signToken(user);
+    res.json({ token, user });
+  },
+  // login a user, sign a token, and send it back (to client/src/components/LoginForm.js)
+  // {body} is destructured req.body
+  async login({ body }, res) {
+    const user = await User.findOne({ $or: [{ username: body.username }, { email: body.email }] });
+    if (!user) {
+      return res.status(400).json({ message: "Can't find this user" });
+    }
+
+    const correctPw = await user.isCorrectPassword(body.password);
+
+    if (!correctPw) {
+      return res.status(400).json({ message: 'Wrong password!' });
+    }
+    const token = signToken(user);
+    res.json({ token, user });
+  },
+// Save a book to a user's profile ----------------------------------------- Everything here down will need to be rewritten/adjusted 
+// async saveBook({ user, body }, res) { 
+//   try {
+//     const updatedUser = await User.findOneAndUpdate(
+//       { _id: user._id },
+//       { $addToSet: { savedBooks: body } },
+//       { new: true, runValidators: true }
+//     );
+//     res.json(updatedUser);
+//   } catch (err) {
+//     console.error('Error saving book:', err);
+//     res.status(500).json(err);
+//   }
+// },
+
+// // Remove a book from a user's profile
+// async deleteBook({ user, params }, res) {
+//   try {
+//     const updatedUser = await User.findOneAndUpdate(
+//       { _id: user._id },
+//       { $pull: { savedBooks: { bookId: params.bookId } } },
+//       { new: true }
+//     );
+//     res.json(updatedUser);
+//   } catch (err) {
+//     console.error('Error deleting book:', err);
+//     res.status(500).json(err);
+//   }
+// },
+};
