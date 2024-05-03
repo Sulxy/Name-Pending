6157f43eb850399e4b428cae184786132141bcbf
SamBishop06 <samuelbishop06@yahoo.com>
2024-05-03 13:37:48 -0500

    resolver.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,20 +1,30 @@
+// Importing necessary models
 const { User, Post, Attachment } = require('../models');
 
+// Defining resolvers for GraphQL queries
 const resolvers = {
-    Query: {
-      users: async () => {
-        return await User.find({}).populate('post').populate({
-          path: 'post',
-          populate: 'attachment'
-        });
+  Query: {
+    // Resolver for fetching users with their posts and attachments
+    users: async () => {
+      // Finding all users and populating their 'post' field with associated posts
+      return await User.find({}).populate('post').populate({
+        // Populating 'post' field with 'attachment' field of each post
+        path: 'post',
+        populate: 'attachment'
+      });
     },
+    // Resolver for fetching posts with their attachments
     post: async () => {
+      // Finding all posts and populating their 'attachment' field
       return await Post.find({}).populate('attachment');
     },
+    // Resolver for fetching attachments
     attachment: async () => {
+      // Finding all attachments
       return await Attachment.find({});
-      }
     }
-  };
+  }
+};
 
-  module.exports = resolvers;
+// Exporting resolvers
+module.exports = resolvers;
