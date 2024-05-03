eeb6f6e1c87d6ccffd9895127415efb9f2b389fa
SamBishop06 <samuelbishop06@yahoo.com>
2024-05-03 13:26:01 -0500

    resolver.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,20 +1,20 @@
 const { User, Post, Attachment } = require('../models');
 
 const resolvers = {
-  Query: {
-    users: async () => {
-      return await User.find({}).populate('post').populate({
-        path: 'post',
-        populate: 'attachment'
-      });
-  },
-  post: async () => {
-    return await Post.find({}).populate('attachment');
-  },
-  attachment: async () => {
-    return await Attachment.find({});
+    Query: {
+      users: async () => {
+        return await User.find({}).populate('post').populate({
+          path: 'post',
+          populate: 'attachment'
+        });
+    },
+    post: async () => {
+      return await Post.find({}).populate('attachment');
+    },
+    attachment: async () => {
+      return await Attachment.find({});
+      }
     }
-  }
-};
+  };
 
-module.exports = resolvers;
+  module.exports = resolvers;
