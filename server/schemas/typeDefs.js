cfda6d1da2f3cb65c079f391cd6bfe2024aae82e
Joshua <Joshuahale829@gmail.com>
2024-05-06 11:29:51 -0500

    Updated typeDefs to include mutations and user/post input.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -11,9 +11,30 @@ const typeDefs = `
     message: String
     timestamp: Date
   }
+
   type Query {
     users: [User]
     posts: [Post]
+  }
+
+  type Mutation {
+    createUser(input: UserInput): User
+    updateUser(id: ID!, input: UserInput): User
+    deleteUser(id: ID!): User
+    createPost(input: PostInput): Post
+    updatePost(id: ID!, input: PostInput): Post
+    deletePost(id: ID!): Post
+
+  input UserInput {
+    username: String
+    email: String
+    password: String
+  }
+
+  input PostInput {
+    user: ID!
+    message: String
+    timestamp: Date
   }
 `;
 
