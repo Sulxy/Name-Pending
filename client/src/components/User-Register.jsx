9005272a644a16fcb5782a4173da5f687a3d60c2
Ray Beliveau <ray@rayxis.com>
2024-05-06 21:31:27 -0500

    Update layout styling and enhance login/register forms
    Added dynamic login/register swap functionality with updated styling in UserLogin and UserRegister components. Modified layout styling in main.scss to improve overall view layout. Added header division with interactive link to switch form views and relevant error message handling to both UserLogin and UserRegister components.

--- client/src/components/User-Register.jsx
+++ client/src/components/User-Register.jsx
@@ -1,57 +1,83 @@
+import React, { useContext, useState } from 'react';
 import { Helmet } from 'react-helmet';
+import { Link } from 'react-router-dom';
+import { AppContext } from '../pages/Home';
 
 // Load CSS
 import '../assets/styles/sections/loginregister.scss';
 
-export default () => (
-	<>
-		<Helmet>
-			<title>Register</title>
-			<meta name="description" content="Create a new account on our site. Enter your personal information and secure your profile."/>
-		</Helmet>
+export default () => {
+	const { setIsLogin }          = useContext(AppContext);
+	const [error, setError]       = useState('');
 
-		<form className="form register-form" onSubmit={e => e.preventDefault()}>
+	const handleFormSubmit = (e) => {
+		e.preventDefault();
+		// TODO: Add Code Here
+	};
 
-			<div className="form__row register-form__row--name">
-				<label className="form__label form__label--name" htmlFor="name">Name</label>
-				<input className="form__input form__input--name" type="text" name="name" placeholder="Full Name"/>
-			</div>
+	return (
+		<>
+			<Helmet>
+				<title>Register</title>
+				<meta name="description"
+				      content="Create a new account on our site. Enter your personal information and secure your profile."/>
+			</Helmet>
 
-			<div className="form__row register-form__row--username">
-				<label className="form__label form__label--username" htmlFor="username">Username</label>
-				<input className="form__input form__input--username" type="text" name="username" placeholder="Username"/>
-			</div>
+			<form className="form register-form" onSubmit={handleFormSubmit}>
+				<header className="form__header">
+					<h2 className="form__title">Register</h2>
+					<Link className="link form__link" onClick={() => setIsLogin(true)} to="#">
+						Go to Login
+					</Link>
+				</header>
 
-			<div className="form__row register-form__row--email">
-				<label className="form__label form__label--email" htmlFor="email">Email</label>
-				<input className="form__input form__input--email" type="email" name="email" placeholder="Email"/>
-			</div>
+				<div className="form__row register-form__row--name">
+					<label className="form__label form__label--name" htmlFor="name">Name</label>
+					<input className="form__input form__input--name" type="text" name="name" placeholder="Full Name"/>
+				</div>
 
-			<div className="form__row register-form__row--password">
-				<label className="form__label form__label--password" htmlFor="password">Password</label>
-				<input className="form__input form__input--password" type="password" name="password" placeholder="Password"/>
-			</div>
+				<div className="form__row register-form__row--username">
+					<label className="form__label form__label--username" htmlFor="username">Username</label>
+					<input className="form__input form__input--username" type="text" name="username"
+					       placeholder="Username"/>
+				</div>
 
-			<div className="form__row register-form__row--confirm">
-				<label className="form__label form__label--confirm-password" htmlFor="confirmpassword">Confirm</label>
-				<input className="form__input form__input--confirm-password" type="password" name="confirmpassword" placeholder="Confirm Password"/>
-			</div>
+				<div className="form__row register-form__row--email">
+					<label className="form__label form__label--email" htmlFor="email">Email</label>
+					<input className="form__input form__input--email" type="email" name="email" placeholder="Email"/>
+				</div>
 
-			<div className="form__row register-form__row--accept-terms">
-				<input className="form__input form__input--checkbox" type="checkbox" name="coppa" id="coppa"/>
-				<label className="form__label form__label--checkbox" htmlFor="coppa">I agree that I am at least 13 years old</label>
-			</div>
+				<div className="form__row register-form__row--password">
+					<label className="form__label form__label--password" htmlFor="password">Password</label>
+					<input className="form__input form__input--password" type="password" name="password"
+					       placeholder="Password"/>
+				</div>
 
-			<div className="form__row register-form__row--accept-terms">
-				<input className="form__input form__input--checkbox" type="checkbox" name="terms" id="terms"/>
-				<label className="form__label form__label--checkbox" htmlFor="terms">I agree to the Terms and Conditions</label>
-			</div>
+				<div className="form__row register-form__row--confirm">
+					<label className="form__label form__label--confirm-password"
+					       htmlFor="confirmpassword">Confirm</label>
+					<input className="form__input form__input--confirm-password" type="password" name="confirmpassword"
+					       placeholder="Confirm Password"/>
+				</div>
 
-			<div className="form__row register-form__row--submit">
-				<input className="form__button form__button--submit" type="submit" value="Register"/>
-			</div>
+				<div className="form__row register-form__row--accept-terms">
+					<input className="form__input form__input--checkbox" type="checkbox" name="coppa" id="coppa"/>
+					<label className="form__label form__label--checkbox"
+					       htmlFor="coppa">I agree that I am at least 13 years old</label>
+				</div>
 
-			<div className="form__message form__message--error">Error message goes here</div>
-		</form>
-	</>
-);
+				<div className="form__row register-form__row--accept-terms">
+					<input className="form__input form__input--checkbox" type="checkbox" name="terms" id="terms"/>
+					<label className="form__label form__label--checkbox"
+					       htmlFor="terms">I agree to the Terms and Conditions</label>
+				</div>
+
+				<div className="form__row register-form__row--submit">
+					<input className="form__button form__button--submit" type="submit" value="Register"/>
+				</div>
+
+				<div className="form__message form__message--error">{error}</div>
+			</form>
+		</>
+	);
+}
