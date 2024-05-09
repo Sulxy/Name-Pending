2d7fd9f4cf2b4870bdbc0f325eebc7d281a2c070
DannySanchez03 <dannyproa303@gmail.com>
2024-05-09 01:18:54 -0500

    Finished with this branch nothing serious to look over

--- client/src/config/ApolloClient.js
+++ client/src/config/ApolloClient.js
@@ -3,8 +3,8 @@ import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-	                                uri: 'http://localhost:3001/graphql', // replace with your GraphQL server URI
-                                });
+	uri: 'http://localhost:3001/graphql', // replace with your GraphQL server URI
+});
 
 const authLink = setContext((_, { headers }) => {
 	const token = localStorage.getItem('auth_token'); // replace 'auth_token' with your token key
@@ -17,8 +17,8 @@ const authLink = setContext((_, { headers }) => {
 });
 
 const client = new ApolloClient({
-	                                link: authLink.concat(httpLink),
-	                                cache: new InMemoryCache(),
-                                });
+	link: authLink.concat(httpLink),
+	cache: new InMemoryCache(),
+});
 
 export default client;
