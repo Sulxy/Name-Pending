a0dda8b13e6bc8c9a7b37c4f7c03b74d69ac7666
Ray Beliveau <ray@rayxis.com>
2024-05-12 17:31:37 -0500

    Refactor SCSS for maintainability and CSS variable usage
    The main.scss, themes.scss, home.scss, _values.scss, and other SCSS component styles have been significantly refactored. The most substantial change is the shift towards using CSS variables declared in themes for dynamic theme adjustments. The _mixins.scss file also added some new mixins and updated existing ones. Revisions were done to maintain code cleanliness and improve flexibility for future modifications.

--- client/src/components/LanguageSwitcher.jsx
+++ client/src/components/LanguageSwitcher.jsx
@@ -18,14 +18,14 @@ export default () => {
 	};
 
 	// useMemo will only recompute the memoized value when one of the dependencies has changed.
-	const flags = React.useMemo(() => {
+	const classNames = React.useMemo(() => {
 		return Object.entries(settings.locale.list).map(([key, flag]) => {
 			return {
 				key, flag,
 				// Generate classNames using template strings and array join
-				className: ['languages__flag',
-				            `languages__flag--${flag}`,
-				            activeLanguage === flag ? 'active' : ''].join(' '),
+				classNames: ['languages__flag',
+				             `languages__flag--${flag}`,
+				             activeLanguage === flag ? 'active' : ''].join(' '),
 				// Get the translated label for the flag
 				label:     t(`languages.aria.${flag}`)
 			};
@@ -37,14 +37,13 @@ export default () => {
 		<div className="languages">
 			<header className="languages__header">{t('languages.title')}</header>
 			{/* Loop through the memoized `flags` array, and for each flag, create a span element with the flag's key, className, and label */}
-			{flags.map(({ key, flag, classNames, label }) => (
+			{classNames.map(({ key, flag, classNames, label }) => (
 				<span
 					onClick={() => handleLanguageChange(flag)}
 					className={classNames}
 					role="img"
 					key={key}
-					aria-label={label}>
-        </span>
+					aria-label={label}></span>
 			))}
 		</div>
 	);
